05b68a9456f520fd7a5c61ca08c0c64d77f80892
muhAkbar07 <muhakbar072x@gmail.com>
2024-04-05 07:05:26 +0700

    merubah responsive

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz9oMIgczc2I-JEHJEaAV3lZ9Fb6cN2Am9t_6BGWh96F0dQYY7ptdFo09PoZ4RUWZO7/exec'
-const form = document.forms['submit-to-google-sheet']
+const scriptURL = "https://script.google.com/macros/s/AKfycbz9oMIgczc2I-JEHJEaAV3lZ9Fb6cN2Am9t_6BGWh96F0dQYY7ptdFo09PoZ4RUWZO7/exec";
+const form = document.forms["submit-to-google-sheet"];
 
-$(".x").click(()=>{
-  $(".alert").toggleClass("d-none")
-})
+$(".x").click(() => {
+  $(".alert").toggleClass("d-none");
+});
 
+form.addEventListener("submit", (e) => {
+  $(".buttonload").toggleClass("d-none");
+  $(".submit").toggleClass("d-none");
 
-form.addEventListener('submit', e => {
+  e.preventDefault();
 
-  $(".buttonload").toggleClass('d-none')
-  $(".submit").toggleClass('d-none')
-
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      $(".buttonload").toggleClass('d-none')
-      $(".submit").toggleClass('d-none')
-      $(".alert").toggleClass("d-none")
-      form.reset()
-      console.log('Success!', response)
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", scriptURL);
+  xhr.onload = function () {
+    $(".buttonload").toggleClass("d-none");
+    $(".submit").toggleClass("d-none");
+    $(".alert").toggleClass("d-none");
+    form.reset();
+    console.log("Success!", xhr.responseText);
+  };
+  xhr.onerror = function () {
+    console.error("Error!", xhr.statusText);
+  };
+  xhr.send(new FormData(form));
+});
